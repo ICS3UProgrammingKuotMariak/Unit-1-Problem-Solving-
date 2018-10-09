59eb36b7717b0b64ba518b381fcd6414c5246f13
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-10-09 10:07:14 -0400

    Header comment moving cat

--- MovingCat(KuotM)/MovingCat(KuotM)/MovingCatForm.cs
+++ MovingCat(KuotM)/MovingCat(KuotM)/MovingCatForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Kuot Mariak
+ * Created on: 17-09-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #9 - Moving Cat
+ * Moving Cat KuotM
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
