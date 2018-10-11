0edbaad2224ef10e4bb95ba59eeb8797e3f629a1
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-10-11 11:06:50 -0400

    Tough

--- PizzaCost(KuotM)/PizzaCost(KuotM)/PizzaCostForm.cs
+++ PizzaCost(KuotM)/PizzaCost(KuotM)/PizzaCostForm.cs
@@ -26,8 +26,6 @@ namespace PizzaCost_KuotM_
 
         private void frmPizzaCost_Load(object sender, EventArgs e)
         {
-            // This declares the variables
-            double diameter, tax, subtotal, total
 
             lblSubtotal.Hide();
             lblSubtotalAnswer.Hide();
@@ -39,9 +37,40 @@ namespace PizzaCost_KuotM_
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            // This declares the variables
+            double diameter, tax, subtotal, total;
+
+            // This sets the maximun and minimum values
+            nudDiameter.Minimum = 6;
+            nudDiameter.Maximum = 22;
+
+            // This converts the string from the nud to a double
+            diameter = double.Parse(nudDiameter.Text);
 
             // Calculates Subtotal
-            nudDiameter.number 
+            subtotal = 0.99 + 0.75 + 0.50 * diameter;
+
+            // Calculates Tax
+            tax = subtotal * 0.13;
+
+            // Calculates Total
+            total = subtotal * 1.13;
+
+            // This converts the subtotal, tax and total to their respective labels
+            lblSubtotalAnswer.Text = Convert.ToString(subtotal) + "$";
+            lblTaxAnswer.Text = Convert.ToString(tax) + "$";
+            lblTotalAnswer.Text = Convert.ToString(total) + "$";
+
+            // This shows the labels
+            lblSubtotal.Hide();
+            lblSubtotalAnswer.Hide();
+            lblTax.Hide();
+            lblTaxAnswer.Hide();
+            lblTotal.Hide();
+            lblTotalAnswer.Hide();
+
+
+
 
 
 
